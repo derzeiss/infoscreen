094867bb07cb4d46180101adca393c5670824119
derzeiss <zeiss_alex@web.de>
2017-11-08 16:20:03 +0100

    [FIXED] optimized impression display

--- app/info-impression-container/info-impression-container.component.js
+++ app/info-impression-container/info-impression-container.component.js
@@ -11,39 +11,60 @@ infoImpressionContainerController.$inject = ['$timeout', 'CONFIG', 'Impression']
 function infoImpressionContainerController($timeout, CONFIG, Impression) {
     var ctrl = this;
 
-    ctrl.impressions = updateImpressions(true);
+    ctrl.impressions = [];
     ctrl.visibleImpression = -1;
+
+    updateImpressions(true);
     ////////////////////
 
     function updateImpressions(initialUpdate) {
-        if(CONFIG.clientDebug) console.log('[infoImpressionContainer][controller][updateImpressions] initialUpdate:', initialUpdate);
-
-        // fetch impressions via $resource
-        var impressions = Impression.query();
+        if (CONFIG.clientDebug) console.log('[infoImpressionContainer][controller][updateImpressions] initialUpdate:', initialUpdate);
 
         // set isVisible property to false for all impressions when they're fetched
-        impressions.$promise.then(function () {
-            if(!ctrl.impressions.length) return;
+        Impression.query().$promise.then(function (impressions) {
+            if (!impressions.length) return;
+
+            // make old impressions obsolete
             ctrl.impressions.forEach(function (impression) {
-                impression.isVisible = false;
+                impression.isObsolete = true;
             });
-            showNextImpression();
+            impressions.forEach(function (impression) {
+                ctrl.impressions.push(impression);
+            });
+
+            // setup timeout for next impression-update
+            $timeout(updateImpressions, CONFIG.impressionRefreshDuration);
+
+            // start animation loop if it's the first update
+            if(initialUpdate) showNextImpression();
         });
-
-        // set timeout for next execution if it's the initial update
-        if(initialUpdate) $timeout(updateImpressions, CONFIG.impressionRefreshDuration);
-
-        return impressions;
     }
 
     function showNextImpression() {
-        var imp = ctrl.impressions,
-            currImp = imp[ctrl.visibleImpression];
-        if(currImp) currImp.isVisible = false;  // hide current impression
-        if (++ctrl.visibleImpression >= imp.length) ctrl.visibleImpression = 0; // change to next impression
-        currImp = imp[ctrl.visibleImpression];
-        currImp.isVisible = true;   // show next impression
-        $timeout(showNextImpression, currImp.duration * 1000);
+        var currImp, nextImp;
+
+        // set current impression
+        currImp = ctrl.impressions[ctrl.visibleImpression] || {};
+
+        // calc index of next impression and set
+        ctrl.visibleImpression = (ctrl.visibleImpression + 1) % ctrl.impressions.length;
+        nextImp = ctrl.impressions[ctrl.visibleImpression];
+
+        // swap impressions
+        if (currImp) currImp.isVisible = false;
+        nextImp.isVisible = true;
+
+        // setup timeout for next swap
+        $timeout(showNextImpression, nextImp.duration * 1000);
+
+        // plan to remove old impression if it's obsolete
+        if(currImp.isObsolete) {
+            $timeout(function() {
+                var index = ctrl.impressions.indexOf(currImp);
+                if(index > -1) ctrl.impressions.splice(index, 1);
+                ctrl.visibleImpression = Math.max(ctrl.visibleImpression - 1, 0);
+            }, 2000);
+        }
     }
 
 }
